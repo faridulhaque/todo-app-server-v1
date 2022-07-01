3093f592a0b1bf5665bb58c5dd08e3775c74500a
faridulhaque <faridulhaquemurshed@gmail.com>
2022-07-02 03:33:58 +0600

    almost done

--- index.js
+++ index.js
@@ -22,11 +22,18 @@ async function run() {
   try {
     await client.connect();
     const tasksCollection = client.db("myTodo").collection("tasks");
+    const completedCollection = client.db("myTodo").collection("completed");
 
     //   posting added tasks in db
     app.post("/tasks", async (req, res) => {
       const tasks = req.body;
       const result = await tasksCollection.insertOne(tasks);
+      res.send(result);
+    });
+    // posting completed task
+    app.post("/completed", async (req, res) => {
+      const completed = req.body;
+      const result = await completedCollection.insertOne(completed);
       res.send(result);
     });
     //   getting data for home page tasks
@@ -36,6 +43,50 @@ async function run() {
       const query = { email: email };
       const cursor = tasksCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    //having completed tasks collection
+    app.get("/completed", async (req, res) => {
+      const email = req.query.email;
+
+      const query = { email: email };
+      const cursor = completedCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // getting single data for editing page
+    app.get("/taskediting/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await tasksCollection.findOne(query);
+      res.send(result);
+    });
+    // removing data from task collections
+    app.delete("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await tasksCollection.deleteOne(query);
+      res.send(result);
+    });
+    // editing a task
+    app.put("/editTask/:email", async (req, res) => {
+      const email = req.params.email;
+      
+      const user = req.body;
+     
+      const query = { email: email };
+      const options = { upsert: true };
+      const info = {
+        $set: {
+          email: user.email,
+          task: user.task,
+          category: user.category,
+          time: user.time,
+          newDate: user.newDate,
+        },
+      };
+      const result = await tasksCollection.updateOne(query, info, options);
+
       res.send(result);
     });
   } finally {
